6df5aa7795a9f417f27022821b46f4b16a3d09b7
duncangraham <duncan@mapbox.com>
2015-01-30 16:19:32 -0800

    helpscout stuff

---
+++ helpscout/tileJSONlayers.js
@@ -0,0 +1,89 @@
+var pontosExtTilesJSON = {
+
+"attribution":"",
+"bounds":[-46.832,-24.0164,-46.3568,-23.3536],
+"center":[-46.67,-23.6266,14],
+"description":"",
+"download":"http://a.tiles.mapbox.com/v4/skualos.tnee8yrp.mbtiles?access_token=pk.eyJ1Ijoic2t1YWxvcyIsImEiOiJUaXo3NjNJIn0.MKxvY63QeYiFnj0lCYPmhA",
+"filesize":266993664,
+"format":"png",
+"id":"skualos.tnee8yrp",
+"mapbox_logo":true,
+"maxzoom":16,
+"minzoom":11,
+"name":
+"pontox_ext_tm1_lin",
+"private":true,
+"scheme":"xyz",
+"template":"",
+"tilejson":"2.0.0",
+"tiles":["http://a.tiles.mapbox.com/v4/skualos.tnee8yrp/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2t1YWxvcyIsImEiOiJUaXo3NjNJIn0.MKxvY63QeYiFnj0lCYPmhA","http://b.tiles.mapbox.com/v4/skualos.tnee8yrp/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2t1YWxvcyIsImEiOiJUaXo3NjNJIn0.MKxvY63QeYiFnj0lCYPmhA"],
+"version":"1.0.0",
+"webpage":"http://a.tiles.mapbox.com/v4/skualos.tnee8yrp/page.html?access_token=pk.eyJ1Ijoic2t1YWxvcyIsImEiOiJUaXo3NjNJIn0.MKxvY63QeYiFnj0lCYPmhA"
+}
+
+ var mapboxStreetTilesJSON = {
+
+ "attribution":"<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>",
+ "autoscale":true,
+ "bounds":[-180,-85,180,85],
+ "center":[-74.00769352912903,40.74101426921151,16],
+ "data":["http://a.tiles.mapbox.com/v3/skualos.kd94ob54/markers.geojsonp"],
+ "dataset":"skualos.kd951m3a",
+ "description":"",
+ "embed":"http://a.tiles.mapbox.com/v3/skualos.kd94ob54.html",
+ "geocoder":"http://a.tiles.mapbox.com/v3/skualos.kd94ob54/geocode/{query}.jsonp",
+ "id":"skualos.kd94ob54",
+ "maxzoom":16,
+ "minzoom":11,
+ "name":"mapbox-streets",
+ "private":true,
+ "scheme":"xyz",
+ "tilejson":"2.0.0",
+ "tiles":["http://a.tiles.mapbox.com/v3/skualos.kd94ob54/{z}/{x}/{y}.png","http://b.tiles.mapbox.com/v3/skualos.kd94ob54/{z}/{x}/{y}.png"],
+ "webpage":"http://a.tiles.mapbox.com/v3/skualos.kd94ob54/page.html"
+}
+
+ var mapboxDarkTilesJSON = {
+
+ "attribution":"<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>",
+ "autoscale":true,
+ "bounds":[-180,-85.0511,180,85.0511],
+ "center":[-46.672325134277344,-23.603003449227153,11],
+ "data":["http://a.tiles.mapbox.com/v3/skualos.kp3l5nam/markers.geojsonp"],
+ "dataset":"skualos.kp3l8ce9",
+ "description":"",
+ "embed":"http://a.tiles.mapbox.com/v3/skualos.kp3l5nam.html",
+ "id":"skualos.kp3l5nam",
+ "maxzoom":16,
+ "minzoom":11,
+ "name":"mapbox-dark",
+ "private":true,
+ "scheme":"xyz",
+ "tilejson":"2.0.0",
+ "tiles":["http://a.tiles.mapbox.com/v3/skualos.kp3l5nam/{z}/{x}/{y}.png","http://b.tiles.mapbox.com/v3/skualos.kp3l5nam/{z}/{x}/{y}.png"],
+ "webpage":"http://a.tiles.mapbox.com/v3/skualos.kp3l5nam/page.html"
+}
+
+var mapboxSatTilesJSON 	=  {
+
+"attribution":"<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>",
+"autoscale":true,
+"bounds":[-180,-85.0511,180,85.0511],
+"center":[-46.674041748046875,-23.565088554605992,12],
+"data":["http://a.tiles.mapbox.com/v3/skualos.l0i6ap5l/markers.geojsonp"],
+"dataset":"skualos.l0i6dg0l",
+"description":"",
+"embed":"http://a.tiles.mapbox.com/v3/skualos.l0i6ap5l.html",
+"id":"skualos.l0i6ap5l",
+"maxzoom":16,
+"minzoom":11,
+"name":"mapbox-sat-streets",
+"private":true,
+"scheme":"xyz",
+"tilejson":"2.0.0",
+"tiles":["http://a.tiles.mapbox.com/v3/skualos.l0i6ap5l/{z}/{x}/{y}.png","http://b.tiles.mapbox.com/v3/skualos.l0i6ap5l/{z}/{x}/{y}.png"],
+"webpage":"http://a.tiles.mapbox.com/v3/skualos.l0i6ap5l/page.html"
+}
+
+
